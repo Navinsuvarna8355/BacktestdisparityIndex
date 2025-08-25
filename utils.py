c117840c712aefed0cc2d6e3b110f53dd31e1f75
Navinsuvarna8355 <156652548+Navinsuvarna8355@users.noreply.github.com>
2025-08-25 16:17:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import requests
 import json
+from datetime import datetime
 
-def fetch_option_chain(symbol="NIFTY"):
+def fetch_option_chain(symbol="BANKNIFTY"):
     url = f"https://www.nseindia.com/api/option-chain-indices?symbol={symbol}"
     headers = {
         "User-Agent": "Mozilla/5.0",
@@ -28,18 +29,25 @@ def parse_option_chain(data):
         ce = item.get("CE", {})
         pe = item.get("PE", {})
         ce_oi = ce.get("openInterest", 0)
-        pe_oi = pe.get("openInterest", 0)
+        ce_chng_oi = ce.get("changeinOpenInterest", 0)
         ce_iv = ce.get("impliedVolatility", None)
+
+        pe_oi = pe.get("openInterest", 0)
+        pe_chng_oi = pe.get("changeinOpenInterest", 0)
         pe_iv = pe.get("impliedVolatility", None)
 
         ce_oi_total += ce_oi
         pe_oi_total += pe_oi
+
         strikes.append({
             "Strike": strike,
             "CE_OI": ce_oi,
+            "CE_Chng_OI": ce_chng_oi,
             "PE_OI": pe_oi,
+            "PE_Chng_OI": pe_chng_oi,
             "CE_IV": ce_iv,
-            "PE_IV": pe_iv
+            "PE_IV": pe_iv,
+            "Timestamp_IST": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         })
 
     pcr = round(pe_oi_total / ce_oi_total, 2) if ce_oi_total else None
